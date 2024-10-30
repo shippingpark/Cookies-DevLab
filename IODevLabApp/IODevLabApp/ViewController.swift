9b771a0b77c630963deba60af96ac66236478e82
Anjin Lee <anjin720jin@naver.com>
2024-10-31 03:59:06 +0900

    Feat: 지출 버튼을 눌렀을 때 텍스트필드의 값을 지출 값으로 보이기 일단 성공

--- IODevLabApp/IODevLabApp/ViewController.swift
+++ IODevLabApp/IODevLabApp/ViewController.swift
@@ -10,11 +10,11 @@ import UIKit
 class ViewController: UIViewController {
     
     @IBOutlet weak var spendInputView: UIView! // 지출 TextField + 지출 버튼
+    @IBOutlet weak var spendTextField: UITextField! // 텍스트필드
     @IBOutlet weak var spendButton: UIButton! // 지출 버튼
     
     @IBOutlet weak var spendLabelView: UIView! // 지출한 값 + "원"
     @IBOutlet weak var spendLabel: UILabel! // 지출한 값
-    
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -26,6 +26,16 @@ class ViewController: UIViewController {
         
         // 지출 버튼 radius 지정
         spendButton.layer.cornerRadius = 16
+    }
+    
+    // 지출 버튼 action
+    @IBAction func spend(_ sender: UIButton) {
+        print("spend")
+        // 지출 텍스트필드의 값을 지출 레이블로 가져온다
+        spendLabel.text = spendTextField.text
+        
+        // 지출 텍스트필드의 값을 지워준다
+        spendTextField.text = ""
     }
 }
 
