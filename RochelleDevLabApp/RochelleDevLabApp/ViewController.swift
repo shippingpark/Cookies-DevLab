707e9fe1cbe927a4a516654bdfbc442364eab6b2
Rochelle0922 <yeonjun6193@naver.com>
2024-10-31 04:03:31 +0900

    [Feat]화면 디자인 완성 및 기능 구현

--- RochelleDevLabApp/RochelleDevLabApp/ViewController.swift
+++ RochelleDevLabApp/RochelleDevLabApp/ViewController.swift
@@ -8,12 +8,36 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+  
+  @IBOutlet weak var textField: UITextField!
+  
+  @IBOutlet weak var afterButtonPressed: UIView!
+  
+  @IBOutlet weak var resultLabel: UILabel!
+  
+  
+  
   override func viewDidLoad() {
     super.viewDidLoad()
-    // Do any additional setup after loading the view.
+    
+    afterButtonPressed.isHidden = true
+    
   }
-
-
+  
+  @IBAction func buttonPressed(_ sender: UIButton) {
+    
+    afterButtonPressed.isHidden = false
+    
+    
+    if textField.text?.isEmpty == false {
+      let number = Int(textField.text!) ?? 0
+      
+      let numberFormatter = NumberFormatter()
+      numberFormatter.numberStyle = .decimal
+      
+      resultLabel.text = numberFormatter.string(from: NSNumber(value: number))
+    } else {
+      resultLabel.text = "숫자를 입력하세요"
+    }
+  }
 }
-
