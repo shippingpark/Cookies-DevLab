75fb75a98a524859da6eeef07feb0804245d4a20
Anjin Lee <anjin720jin@naver.com>
2024-10-31 03:46:36 +0900

    Feat: 화면을 일단.. 그려보았습니다.. 뭐.. 네..

--- IODevLabApp/IODevLabApp/ViewController.swift
+++ IODevLabApp/IODevLabApp/ViewController.swift
@@ -8,12 +8,24 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var spendInputView: UIView! // 지출 TextField + 지출 버튼
+    @IBOutlet weak var spendButton: UIButton! // 지출 버튼
+    
+    @IBOutlet weak var spendLabelView: UIView! // 지출한 값 + "원"
+    @IBOutlet weak var spendLabel: UILabel! // 지출한 값
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        // 지출 텍스트필드와 값 View radius 지정
+        spendInputView.layer.cornerRadius = 20
+        spendLabelView.layer.cornerRadius = 20
+        
+        // 지출 버튼 radius 지정
+        spendButton.layer.cornerRadius = 16
     }
-
-
 }
 
