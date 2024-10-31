bfd67580df96c6a893bda03675e58f2319663dc4
Anjin Lee <anjin720jin@naver.com>
2024-10-31 20:51:10 +0900

    Feat: error message를 띄우는 레이블 추가 + 텍스트필드 길이 제한

--- IODevLabApp/IODevLabApp/ViewController.swift
+++ IODevLabApp/IODevLabApp/ViewController.swift
@@ -10,9 +10,10 @@ import UIKit
 class ViewController: UIViewController {
     
     @IBOutlet weak var spendInputView: UIView! // 지출 TextField + 지출 버튼
-    // TODO: 텍스트필드에서 입력할 때도 숫자 형식 변환
     @IBOutlet weak var spendTextField: UITextField! // 텍스트필드
     @IBOutlet weak var spendButton: UIButton! // 지출 버튼
+    
+    @IBOutlet weak var errorMessageLabel: UILabel! // 에러 메시지 레이블
     
     @IBOutlet weak var spendLabelView: UIView! // 지출한 값 + "원"
     @IBOutlet weak var spendLabel: UILabel! // 지출한 값
@@ -44,8 +45,8 @@ class ViewController: UIViewController {
                 spendLabel.text = numberFormatter.string(from: spendValue as NSNumber)
                 
             } else { // TextField의 값이 숫자가 아닌 경우
-                // TODO: 숫자가 아님을 경고할 때 텍스트 색상도 변경해보자
-                spendLabel.text = "숫자만 입력하세유~~"
+                errorMessageLabel.text = "숫자만 입력하세유~~"
+                spendLabel.text = "0"
             }
             
             // 지출 텍스트필드의 값을 지워준다
@@ -54,8 +55,22 @@ class ViewController: UIViewController {
         } else { // 텍스트필드가 비어있을 때 동작
             
             // 레이블에 숫자를 입력 후에 누르라는 메시지 전달
-            spendLabel.text = "금액을 입력하고 지출해~~"
+            errorMessageLabel.text = "숫자를 입력해주세유~~"
+            spendLabel.text = "0"
             
+        }
+    }
+    
+    // TextField 입력 길이 제한
+    @IBAction func editingChangedSpendTextField(_ sender: UITextField) {
+        guard let spendTextFieldText = sender.text else { return }
+        
+        if spendTextFieldText.count > 15 {
+            sender.deleteBackward()
+            
+            errorMessageLabel.text = "15자리까지 입력할 수 있어유~~"
+        } else {
+            errorMessageLabel.text = ""
         }
     }
 }
