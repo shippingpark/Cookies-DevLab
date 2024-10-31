077f695d7519a8b3c84e932adef8540cac13ae0e
KIM SEOWOO <sc204016@gmail.com>
2024-10-31 10:05:15 +0900

    [Feat] 지출 텍스트필드 및 입력버튼 코드와 연결

--- TinaDevLabApp/TinaDevLabApp/ViewController.swift
+++ TinaDevLabApp/TinaDevLabApp/ViewController.swift
@@ -9,11 +9,16 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var expenseInput: UITextField!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func expenseTapped(_ sender: UIButton) {
+    }
+    
 }
 
