88aaf4ff3fdaf312848833ab23861a51b7c659b7
KIM SEOWOO <sc204016@gmail.com>
2024-10-31 13:32:42 +0900

    [feat] 텍스트필드 값을 Label에보여주기

--- TinaDevLabApp/TinaDevLabApp/ViewController.swift
+++ TinaDevLabApp/TinaDevLabApp/ViewController.swift
@@ -8,12 +8,20 @@
 import UIKit
 
 class ViewController: UIViewController {
-
-    @IBOutlet weak var expenseInput: UITextField!
     
-    @IBOutlet weak var labelExpenseOutput: UILabel!
+    // 텍스트 필드
+    @IBOutlet weak var expenseInput: UITextField!
+
+    // 숫자 나오는 곳
+    @IBOutlet weak var expenseOutput: UILabel!
+    
     @IBOutlet weak var labelWon: UILabel!
     
+    
+    // 1) 텍스트 필드에 숫자 입력하기
+    // 2) 지출 버튼 누르기
+    // 3) 입력된 숫자가 라벨에 표시되기
+    // 4) 텍스트 필드 숫자 없어지기
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -23,7 +31,12 @@ class ViewController: UIViewController {
    
     // 지출 입력 버튼
     @IBAction func expenseTapped(_ sender: UIButton) {
-        labelExpenseOutput.text = expenseInput.text!
+       
+        // 3) 입력된 숫자가 라벨에 표시되기
+        expenseOutput.text = expenseInput.text
+        
+
+       
     }
     
 }
