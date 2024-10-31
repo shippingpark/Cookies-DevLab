a30d436383b406363f856699d09dcbce13f6e4f4
KIM SEOWOO <sc204016@gmail.com>
2024-10-31 10:41:34 +0900

    [Add] textField를 Label로 변경, 금액 입력 시 금액 표시되는 함수 추가

--- TinaDevLabApp/TinaDevLabApp/ViewController.swift
+++ TinaDevLabApp/TinaDevLabApp/ViewController.swift
@@ -11,17 +11,22 @@ class ViewController: UIViewController {
 
     @IBOutlet weak var expenseInput: UITextField!
     
+    @IBOutlet weak var labelExpenseOutput: UILabel!
+    @IBOutlet weak var labelWon: UILabel!
+    
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
     
+    // 키보드 설정
     func setup() {
         expenseInput.keyboardType = UIKeyboardType.numberPad
     }
-
+    // 지출 입력 버튼
     @IBAction func expenseTapped(_ sender: UIButton) {
+        labelExpenseOutput.text = expenseInput.text!
     }
     
 }
