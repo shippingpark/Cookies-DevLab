eac2d139b8b4efecadbd4e14f1cafaa79b930b09
Seowoo <sc204016@gmail.com>
2024-11-01 02:20:00 +0900

    [Add] Cocoa Touch Class 생성

---
+++ TinaDevLabApp/TinaDevLabApp/UITableViewCell.swift
@@ -0,0 +1,23 @@
+//
+//  UITableViewCell.swift
+//  TinaDevLabApp
+//
+//  Created by KIM SEOWOO on 11/1/24.
+//
+
+import UIKit
+
+class UITableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
