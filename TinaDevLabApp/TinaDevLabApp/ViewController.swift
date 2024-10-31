f92c9ca57f9ec87d266b83f8cead3e92c4f480e0
Seowoo <sc204016@gmail.com>
2024-10-31 23:24:50 +0900

    [Feat] Day1 복기 / textField 크기 변경

--- TinaDevLabApp/TinaDevLabApp/ViewController.swift
+++ TinaDevLabApp/TinaDevLabApp/ViewController.swift
@@ -9,37 +9,39 @@ import UIKit
 
 class ViewController: UIViewController {
     
-    // 텍스트 필드
-    @IBOutlet weak var expenseInput: UITextField!
-
-    // 숫자 나오는 곳
-    @IBOutlet weak var expenseOutput: UILabel!
-    
-    @IBOutlet weak var labelWon: UILabel!
-    
-    
     // 1) 텍스트 필드에 숫자 입력하기
-    // 2) 지출 버튼 누르기
-    // 3) 입력된 숫자가 라벨에 표시되기
-    // 4) 텍스트 필드 숫자 없어지기
+// 2) 지출 버튼 누르기
+// 3) 입력된 숫자가 라벨에 표시되기
+// 4) 텍스트 필드 숫자 없어지기
+    // 변수는 컴포넌트명
+    
+    
+    @IBOutlet weak var spendTextField: UITextField!
+    @IBOutlet weak var spendTextView: UIView!
+    @IBOutlet weak var spendOutputLabel: UILabel!
+    @IBOutlet weak var spendOutputView: UIView!
+    @IBOutlet weak var spendTappedButton: UIButton!
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        // 지출 텍스트필드와 값 view radius 지정
+        spendTextView.layer.cornerRadius = 20
+        
+        spendTappedButton.layer.cornerRadius = 15
+        
+        spendOutputView.layer.cornerRadius = 20
+        
     }
     
    
     // 지출 입력 버튼
-    @IBAction func expenseTapped(_ sender: UIButton) {
-       
+    // 함수는 동사형 시작
+    @IBAction func spendTapped(_ sender: UIButton) {
         // 3) 입력된 숫자가 라벨에 표시되기
-        expenseOutput.text = expenseInput.text
-        
-        
-        
-
-       
+        spendOutputLabel.text = spendTextField.text
     }
+
     
 }
 
