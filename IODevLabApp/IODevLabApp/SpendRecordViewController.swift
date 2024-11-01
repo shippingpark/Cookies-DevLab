2883b1376ee84b82061516e1ff9078417cc65e85
Anjin Lee <anjin720jin@naver.com>
2024-11-01 21:47:39 +0900

    Rename: CodeVC 이름을 SpendRecordVC로 변경

---
+++ IODevLabApp/IODevLabApp/SpendRecordViewController.swift
@@ -0,0 +1,17 @@
+//
+//  SpendRecordViewController.swift
+//  IODevLabApp
+//
+//  Created by Anjin on 11/1/24.
+//
+
+import UIKit
+
+class SpendRecordViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        
+    }
+}
