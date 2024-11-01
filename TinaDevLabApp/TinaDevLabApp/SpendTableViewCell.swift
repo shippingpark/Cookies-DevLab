ba43571d9fee23d0d0a353b9025be83ad7284fbc
Seowoo <sc204016@gmail.com>
2024-11-02 05:15:03 +0900

    [Design] SpendTableViewCell 파일 생성

---
+++ TinaDevLabApp/TinaDevLabApp/SpendTableViewCell.swift
@@ -0,0 +1,30 @@
+//
+//  SpendTableViewCell.swift
+//  TinaDevLabApp
+//
+//  Created by KIM SEOWOO on 11/2/24.
+//
+
+import UIKit
+
+final class MyTableViewCell: UITableViewCell {
+    
+    let deleteButton: UIButton = {
+        let button = UIButton()
+        return button
+        
+    }()
+    
+    let spendRecordsLabel: UILabel = {
+        let label = UILabel()
+        return label
+        
+    }()
+    
+    let spendUnitLabel: UILabel = {
+        let label = UILabel()
+        return label
+    }()
+    
+}
+    
