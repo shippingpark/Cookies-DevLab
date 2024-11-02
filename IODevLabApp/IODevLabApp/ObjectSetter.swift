093e87ccb7ab59459ca76220b456f0063f83b87e
Anjin Lee <anjin720jin@naver.com>
2024-11-02 17:57:51 +0900

    Feat: set 메소드에 self.translatesAutoresizingMaskIntoConstraints = false 추가

--- IODevLabApp/IODevLabApp/ObjectSetter.swift
+++ IODevLabApp/IODevLabApp/ObjectSetter.swift
@@ -5,15 +5,16 @@
 //  Created by Anjin on 11/2/24.
 //
 
-import Foundation
+import UIKit
 
 // set을 구현
 // 이전에 사용해본 then 대신에 직접 구현
 protocol ObjectSetter {}
 
-extension ObjectSetter where Self: AnyObject {
+extension ObjectSetter where Self: UIView {
     func set(_ configure: (Self) -> Void) -> Self {
         configure(self)
+        self.translatesAutoresizingMaskIntoConstraints = false
         return self
     }
 }
