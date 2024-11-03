7d15d6d5f085517b50074f775e37e8f817256657
Rochelle0922 <yeonjun6193@naver.com>
2024-11-03 19:03:39 +0900

    [UI]SwiftUI 뷰 생성(데이터X)

---
+++ RochelleDevLabApp/RochelleDevLabApp/SwiftUIView.swift
@@ -0,0 +1,26 @@
+//
+//  SwiftUIView.swift
+//  RochelleDevLabApp
+//
+//  Created by 이연정 on 11/3/24.
+//
+
+import SwiftUI
+
+struct SwiftUIView: View {
+    var body: some View {
+        HStack(spacing: 10) {
+            Text("금액 입력")
+                .font(.body)
+                .foregroundColor(.primary)
+        }
+        .padding()
+        .frame(minWidth: 343)
+        .background(Color.gray.opacity(0.2))
+        .clipShape(RoundedRectangle(cornerRadius: 999)) // 매우 큰 값으로 모서리를 둥글게
+    }
+}
+
+#Preview {
+    SwiftUIView()
+}
